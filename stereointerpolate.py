f286e3cbdae7e40b5e3f33be792e8e9c0a4a441b
Michael Gschwandtner <mgschwan@zero997.com>
2012-08-14 14:34:33 +0200

    Switched completely to the new OpenCV python interface TODO: The optical Flow detector can no yet be restricted to searches in the x direction alone

--- stereointerpolate.py
+++ stereointerpolate.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python
 import sys
-
-#TODO: REMOVE
-sys.path.insert(0,"/opt/opencv/lib/python2.7/dist-packages/")
-#
-
-
 import numpy
-import cv
 import cv2
 from optparse import OptionParser
 
@@ -236,9 +229,6 @@ if __name__=="__main__":
             left,right = right,left
             left_orig,right_orig = right_orig,left_orig
             
-
-    print "%d,%d %d,%d"%(left.shape[1],left.shape[0], right.shape[1],right.shape[0])
-
     # Handle the image shift
     left_size = (0,0,left.shape[1]-imageshift_x, left.shape[0]-imageshift_y)
     right_size = (imageshift_x,imageshift_y,right.shape[1], right.shape[0])
@@ -252,18 +242,32 @@ if __name__=="__main__":
 
     if cropsize[2] < 0:
         if cmdline["autocrop"] == 1:
-            cropsize = [2*bsize[0],2*bsize[1],left.shape[1]-2*bsize[0],left.shaoe[0]-2*bsize[1]]
+            cropsize = [2*bsize[0],2*bsize[1],left.shape[1]-2*bsize[0],left.shape[0]-2*bsize[1]]
         else:
             cropsize = [0,0,left.shape[1],left.shape[0]]
 
 
    
     
-    flow = cv2.calcOpticalFlowFarneback(left, right, pyr_scale=0.5, levels=3, winsize=bsize[0], iterations=10, poly_n=5, poly_sigma=1.1, flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
+    #flow = numpy.zeros((left.shape[0],left.shape[1], 2), dtype=numpy.float32)
+    #for y in range(flow.shape[0]/maxrange[1]):
+    #  flow[y*maxrange[1]:(y+1)*maxrange[1],:] = cv2.calcOpticalFlowFarneback(left[y*maxrange[1]:(y+1)*maxrange[1],:], right[y*maxrange[1]:(y+1)*maxrange[1],:], pyr_scale=0.5, levels=3, winsize=bsize[0], iterations=10, poly_n=5, poly_sigma=1.1, flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
+    
+    flow = cv2.calcOpticalFlowFarneback(left, right, pyr_scale=0.5, levels=3, winsize=bsize[0], iterations=10, poly_n=5, poly_sigma=1.1, flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN, flow=None)
+    
+    
     velx = flow[:,:,0]
     vely = flow[:,:,1]
-    print str(flow)
 
+    #TODO: Limit the searchrange in the optical flow detector
+    # simply clamping the motion will yield inferior results
+    velx[velx > maxrange[0]] = maxrange[0]
+    velx[velx < -maxrange[0]] = -maxrange[0]
+
+    vely[vely > maxrange[1]] = maxrange[1]
+    vely[vely < -maxrange[1]] = -maxrange[1]
+    
+    
     if filtersize > 0:
         (tmp_velx, tmp_vely) = filterVelocityField(velx,vely, filtersize)
         velx = tmp_velx
@@ -272,7 +276,11 @@ if __name__=="__main__":
     intermediate_frames = []
     flow_frames = []
     if output_mode == "composite":
-        out_composite_image = cv.CreateMat(cropsize[3], (steps+1)*cropsize[2], left_orig.type)
+        shape = list(left_orig.shape)
+        shape[0] = cropsize[3]
+        shape[1] = (steps+1)*cropsize[2]
+        out_composite_image = numpy.zeros(shape)
+
 
 
     for d in range (0, steps+1):
@@ -286,9 +294,7 @@ if __name__=="__main__":
         delta = float(d)/steps
         new_velx = delta * velx
         new_vely = delta * vely
-        print str(new_velx)
 
-        index = 0
 
         #Store all blocks that are moved, to sort them according to their
         #distance to the camera
@@ -303,80 +309,67 @@ if __name__=="__main__":
                 ytpos = ypos + new_vely[y,x]
                 xfinalPos = xpos + velx[y,x]
                 yfinalPos = ypos + vely[y,x]
-                movements.append( ( new_velx[y,x]**2+new_vely[y,x]**2, xpos, ypos, xtpos, xtpos, yfinalPos, yfinalPos ) )
+                """The first element is the x-shift of the block
+                   smaller shifts (maybe even negative) belong to
+                   objects closer to the camera and have to be drawn last"""
+                movements.append( (new_velx[y,x], xpos, ypos, xtpos, ytpos, xfinalPos, yfinalPos ) )
 
+        index = 0
 
+        movements.sort(reverse=True)
 
-        for x in range (0, new_velx.shape[1],shiftsize[0]):
-            for y in range (0, new_velx.shape[0],shiftsize[1]):
-                
+        for m in movements:
                 if index%100 == 0:
-                    print "%.2f %%"%(100.0* (base_progress + float(index)/(float(new_velx.shape[1])*float(new_velx.shape[0])*float(steps+1))) )
+                    print "%.2f %%"%(100.0* (base_progress + float(index)/(len(movements)*float(steps+1))) )
                 index = index +1
+                xpos = m[1]        
+                ypos = m[2]
+                xtpos = m[3]
+                ytpos = m[4]
+                xfinalPos = m[5]
+                yfinalPos = m[6]
+                x_subpixel = xtpos - float(int(xtpos))
 
 
-                xpos = x + bsize[0]/2        
-                ypos = y + bsize[1]/2        
                 cv2.circle(dst, (xpos, ypos), 2, (255,255,255), 1)
-                xtpos = xpos + new_velx[y,x]
-                ytpos = ypos + new_vely[y,x]
-                xfinalPos = xpos + velx[y,x]
-                yfinalPos = ypos + vely[y,x]
-                try:
-                  x_subpixel = xtpos - float(int(xtpos))
-                except:
-                  print "Error: %s (%f,%f) %f"%(str(xtpos), xpos,ypos, new_velx[y,x])
-                if True:
-                #try:
-                    cv2.line(dst, (int(xpos), int(ypos)), (int(xtpos), int(ytpos)), (255,0,0),1)
-                    if xpos - bsize[0]/2 > 0 and xpos + bsize[0]/2 < left_orig.shape[1]-1 and \
-                       ypos- bsize[1]/2 > 0 and ypos + bsize[1]/2 < left_orig.shape[0] and \
-                       xfinalPos - bsize[0]/2 > 0 and xfinalPos + bsize[0]/2 < right_orig.shape[1]-1 and \
-                       yfinalPos- bsize[1]/2 > 0 and yfinalPos + bsize[1]/2 < right_orig.shape[0]-1:
 
-                        
-                        tmpMatLeft = left_orig[ypos- bsize[1]/2:ypos+ bsize[1]/2, xpos - bsize[0]/2:xpos + bsize[0]/2]
-                        tmpMatRight = right_orig[yfinalPos- bsize[1]/2:yfinalPos+ bsize[1]/2, xfinalPos - bsize[0]/2:xfinalPos + bsize[0]/2]    
-                
+                cv2.line(dst, (int(xpos), int(ypos)), (int(xtpos), int(ytpos)), (255,0,0),1)
+                if xpos - bsize[0]/2 > 0 and xpos + bsize[0]/2 < left_orig.shape[1]-1 and \
+                   ypos- bsize[1]/2 > 0 and ypos + bsize[1]/2 < left_orig.shape[0] and \
+                   xfinalPos - bsize[0]/2 > 0 and xfinalPos + bsize[0]/2 < right_orig.shape[1]-1 and \
+                   yfinalPos- bsize[1]/2 > 0 and yfinalPos + bsize[1]/2 < right_orig.shape[0]-1:
+                    
+                    tmpMatLeft = left_orig[ypos- bsize[1]/2:ypos+ bsize[1]/2, xpos - bsize[0]/2:xpos + bsize[0]/2]
+                    tmpMatRight = right_orig[yfinalPos- bsize[1]/2:yfinalPos+ bsize[1]/2, xfinalPos - bsize[0]/2:xfinalPos + bsize[0]/2]    
 
-                        if cmdline["subpixel"]:
-                            tmpMatLeft1 = left_orig[ypos- bsize[1]/2:ypos+ bsize[1]/2, 1+xpos - bsize[0]/2:1+xpos + bsize[0]/2 ]
-                            tmpMatRight1 = right_orig[yfinalPos- bsize[1]/2:yfinalPos+ bsize[1]/2, 1+xfinalPos - bsize[0]/2:1+xfinalPos + bsize[0]/2 ]    
+                    if cmdline["subpixel"]:
+                        tmpMatLeft1 = left_orig[ypos- bsize[1]/2:ypos+ bsize[1]/2, 1+xpos - bsize[0]/2:1+xpos + bsize[0]/2 ]
+                        tmpMatRight1 = right_orig[yfinalPos- bsize[1]/2:yfinalPos+ bsize[1]/2, 1+xfinalPos - bsize[0]/2:1+xfinalPos + bsize[0]/2 ]    
 
-                            block = cv2.addWeighted(tmpMatLeft, 1.0 - (float(d)/float(steps)), tmpMatRight, (float(d)/float(steps)), 0.0) 
-                            block1 = cv2.addWeighted(tmpMatLeft1, 1.0 - (float(d)/float(steps)), tmpMatRight1, (float(d)/float(steps)), 0.0) 
-            
-                            #The destination block is positioned with subpixel accuracy, thats
-                            #why the weighting of the source blocks has to be inverted
-                            # x_subpixel, 1-x_subpixel instead of 1-x_subpixel, x_subpixel
-                            block2 = cv2.addWeighted(block, x_subpixel , block1, 1.0-x_subpixel, 0.0) 
-                            output[int(ytpos)- bsize[1]/2:int(ytpos)+ bsize[1]/2,  int(xtpos) - bsize[0]/2:int(xtpos) + bsize[0]/2] = block2
-                        
-                        else:
-                            b = cv2.addWeighted(tmpMatLeft, 1.0 - (float(d)/float(steps)), tmpMatRight, (float(d)/float(steps)), 0.0) 
-                            output[int(ytpos)- bsize[1]/2:int(ytpos)+ bsize[1]/2,int(xtpos) - bsize[0]/2:int(xtpos) + bsize[0]/2] = b
-                            
+                        block = cv2.addWeighted(tmpMatLeft, 1.0 - (float(d)/float(steps)), tmpMatRight, (float(d)/float(steps)), 0.0) 
+                        block1 = cv2.addWeighted(tmpMatLeft1, 1.0 - (float(d)/float(steps)), tmpMatRight1, (float(d)/float(steps)), 0.0) 
+        
+                        #The destination block is positioned with subpixel accuracy, thats
+                        #why the weighting of the source blocks has to be inverted
+                        # x_subpixel, 1-x_subpixel instead of 1-x_subpixel, x_subpixel
+                        block2 = cv2.addWeighted(block, x_subpixel , block1, 1.0-x_subpixel, 0.0) 
+                        output[int(ytpos)- bsize[1]/2:int(ytpos)+ bsize[1]/2,  int(xtpos) - bsize[0]/2:int(xtpos) + bsize[0]/2] = block2
+                    
+                    else:
+                        b = cv2.addWeighted(tmpMatLeft, 1.0 - (float(d)/float(steps)), tmpMatRight, (float(d)/float(steps)), 0.0) 
+                        output[int(ytpos)- bsize[1]/2:int(ytpos)+ bsize[1]/2,int(xtpos) - bsize[0]/2:int(xtpos) + bsize[0]/2] = b
 
-
-                #except:
-                #    pass # Invalid position
-
-        cv2.imshow("output", numpy.uint8(output))
-        cv2.waitKey(0)
-
-        cv2.imshow("Flow", numpy.uint8(dst))
         flow_frames.append(dst)
-        cropped_out = numpy.copy(output[cropsize[1]:cropsize[3], cropsize[0]:cropsize[2]])
+        cropped_out = numpy.copy(output[cropsize[1]:cropsize[3]+cropsize[1], cropsize[0]:cropsize[2]+cropsize[0]])
         if cmdline["smooth"] > 0:
-            cropped_out = cv2.GaussianBlur( cropped_out, 3, 1)
+            cropped_out = cv2.GaussianBlur( cropped_out, (3,3), 0.5)
 
         intermediate_frames.append(numpy.uint8(cropped_out))
         if output_mode == "composite":
-            out_composite_image[:, d*cropsize[2]:(d+1)*cropsize[2]] = cropped_out
+            out_composite_image[:,d*cropsize[2]:(d+1)*cropsize[2]] = cropped_out
         else:
-            cv2.imwrite("intermediate_%04d.bmp"%d, cropped_out)
-            cv2.imwrite("intermediate_%04d.bmp"%(2*steps - d), cropped_out)
-
+            cv2.imwrite("intermediate_%04d.bmp"%d, numpy.uint8(cropped_out))
+            cv2.imwrite("intermediate_%04d.bmp"%(2*steps - d), numpy.uint8(cropped_out))
 
     if output_mode == "composite":
         cv2.imwrite(output_file, out_composite_image)
@@ -393,4 +386,4 @@ if __name__=="__main__":
 
             cv2.imshow("Output", intermediate_frames[pos])
             cv2.imshow("Flow", flow_frames[pos])
-            cv.WaitKey(20)
+            cv2.waitKey(20)
